e4fe50816d35718971c2106941df7368cd543bfa
zizEast <jonnyashghere96@gmail.com>
2022-09-03 16:26:59 +0200

    primo commit

---
+++ server.js
@@ -0,0 +1,31 @@
+if(process.env.NODE_ENV!='production'){
+    require('dotenv').config()
+}
+
+const express=require('express');
+const app=express();
+
+const expressLayouts=require('express-ejs-layouts');
+const mongoose=require('mongoose');
+
+const routerIndex=require('./router/indexRouter')
+
+mongoose.connect('mongodb+srv://alberto:mongo-123@cluster0.lvmqn.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true});
+
+const db=mongoose.connection;
+
+db.on('error',err=>{throw err})
+db.once('open',()=>{console.log('siamo connessi ad Atlas!');})
+
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views')
+app.set('layout','layouts/layout')
+app.use(expressLayouts);
+
+
+
+app.use(routerIndex)
+
+
+
+app.listen(process.env.PORT||3000)
